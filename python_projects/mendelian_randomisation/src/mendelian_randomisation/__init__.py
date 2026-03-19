724821abb3d95571e52485f50a9ee7428e6f204d
kai <87151509+drkaizeng@users.noreply.github.com>
2026-03-19 20:18:07 +0000

    Add esimate

--- python_projects/mendelian_randomisation/src/mendelian_randomisation/__init__.py
+++ python_projects/mendelian_randomisation/src/mendelian_randomisation/__init__.py
@@ -1,3 +1,4 @@
-from mendelian_randomisation.simulate import run_simulation
+from mendelian_randomisation.estimate import IVWResult, ivw_estimate
+from mendelian_randomisation.simulate import expected_genotype_variance, run_simulation
 
-__all__ = ["run_simulation"]
+__all__ = ["IVWResult", "expected_genotype_variance", "ivw_estimate", "run_simulation"]
